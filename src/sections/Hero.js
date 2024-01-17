9f6c0d0605f0f7d557f761f318c9f2aafd32c88d
AbdurRafay726 <141917365+AbdurRafay726@users.noreply.github.com>
2024-01-17 10:59:12 +0500

    font size

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="mx-auto max-w-screen-1200px  h-screen flex items-center justify-center sm:max-w-600px hero">
-      <div className="text-5xl p-3 m-3 text-mainGreen font-rubik font-bold w-2/3 flex flex-col border-2 border-mainGray md:text-8xl bg-gradient-to-r from-gray-500 via-stone-300 to-gray-500">
+      <div className="text-4xl p-3 m-3 text-mainGreen font-rubik font-bold w-2/3 flex flex-col border-2 border-mainGray xsm:text-5xl sm:text-7xl md:text-8xl bg-gradient-to-r from-gray-500 via-stone-300 to-gray-500">
         <div className="text-left ">
           <div className="typed">Frontend</div>
         </div>
